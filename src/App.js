8b162067808d9e1819ceeb600d36cebfafcf2bc4
Teklehaimanot Belete <enbish.blt@gmail.com>
2022-02-21 10:37:55 +0300

    fetch lists of todo and delete each todo by id

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { MdDelete } from 'react-icons/md';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const getTodos = async () => {
+      const todoFromServer = await fetchTodos();
+      setTodos(todoFromServer);
+    };
+
+    getTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    try {
+      const res = await fetch(
+        'https://jsonplaceholder.typicode.com/todos?userId=2'
+      );
+      const data = await res.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const deleteById = async (id) => {
+    try {
+      const deleteTodo = await fetch(
+        `https://jsonplaceholder.typicode.com/todos/${id}`,
+        {
+          method: 'DELETE',
+        }
+      );
+      setTodos(todos.filter((todo) => id !== todo.id));
+    } catch (error) {}
+  };
+  console.log(todos);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <>
+        {todos.map((todo) => (
+          <div
+            style={{ backgroundColor: todo.completed ? 'yellowgreen' : '' }}
+            key={todo.id}
+            className="TodoList"
+          >
+            <span>{todo.id}</span>
+            <span>{todo.title}</span>
+            <MdDelete
+              style={{ minWidth: '25px' }}
+              onClick={() => deleteById(todo.id)}
+            />
+          </div>
+        ))}
+      </>
     </div>
   );
 }
